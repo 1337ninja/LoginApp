9e808d06bd4059122e4efa764094090bf1b0dd87
1337ninja <1337ninja@users.noreply.github.com>
2017-10-10 22:39:40 +0530

    Initial commit for Qt project.

---
+++ LoginApp/LoginWidget.cpp
@@ -0,0 +1,14 @@
+#include "LoginWidget.h"
+#include "ui_LoginWidget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
---
+++ LoginApp/LoginWidget.h
@@ -0,0 +1,22 @@
+#ifndef LOGINWIDGET_H
+#define LOGINWIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // LOGINWIDGET_H
